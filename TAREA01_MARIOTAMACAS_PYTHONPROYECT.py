f775a1b539ec6d5f3d24cd8ea3045618615b6869
Hemai04 <146993921+Hemai04@users.noreply.github.com>
2023-10-04 13:48:00 -0600

    Add files via upload

---
+++ TAREA01_MARIOTAMACAS_PYTHONPROYECT.py
@@ -0,0 +1,30 @@
+print("I love python!")
+#EJERCICIO 1
+"""
+CREAMOS UN ARCHIVO DE PYTHON
+DONDE ALMACENAREMOS INFORMACION PERSONAL, ESTA INFORMACION DEBE IR CON SU TIPO DE DATO CORRESPONDIENTE
+
+VARIABLES A CREAR
+#NOMBRES
+#PRIMER APELLIDO
+#SEGUNDO APELLIDO
+#EDAD
+#NACIONALIDAD
+#SEXO
+"""
+#variables preasignadas 
+name = str(' Mario Isai')
+lastname = str(' Pineda Tamacas')
+age = int('19')
+nationality = str(' Salvadorean')
+sex = (' Men')
+
+#Imprimimos los datos predefinidos
+print('My name is ' + name + lastname)
+print('i am ', age )
+print('i am ', nationality, 'and i am ',  sex)
+print('This languaje is awesome, is so easy, now i am learning C# and that languaje is so horrible :( ')
+
+
+
+
